9b8de99064c51dbf8a4852a128d99261c7dd06dd
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-05-17 11:42:22 +0000

    Deploy preview for PR #7 🚀

---
+++ preview/7/assets/index-BO2iM_5Z.js
@@ -0,0 +1,1 @@
+import{j as e,M as a,T as s}from"./index-CpoL6C2y.js";const p=()=>e.jsx(a,{title:"Help",children:e.jsx(s,{variant:"body2",children:"Welcome to the Help. This is a sample page."})});export{p as default};
---
+++ preview/7/assets/index-DDfSr8Ax.js
@@ -0,0 +1,1 @@
+import{j as t,M as s,T as e}from"./index-CpoL6C2y.js";const i=()=>t.jsx(s,{title:"Settings",children:t.jsx(e,{variant:"body2",children:"Welcome to the Settings. This is a sample page."})});export{i as default};
