68030ddc08d63b9bdbd9b74212544ddc3d1878f3
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-05-18 11:04:43 +0000

    Deploy preview for PR #11 🚀

---
+++ preview/11/assets/Companies-Dfyf2lBU.js
@@ -0,0 +1,1 @@
+import{j as a,T as s}from"./index-B_UubtJt.js";const o=()=>a.jsx(s,{variant:"body2",children:"Welcome to the Companies. This is a sample page."});export{o as default};
---
+++ preview/11/assets/index-BcxHoyGA.js
@@ -0,0 +1,1 @@
+import{j as e,M as a,T as s}from"./index-B_UubtJt.js";const p=()=>e.jsx(a,{title:"Help",children:e.jsx(s,{variant:"body2",children:"Welcome to the Help. This is a sample page."})});export{p as default};
