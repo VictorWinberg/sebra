42b9c25270d7015c8f647de7fb42b640d518382a
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-05-18 21:13:13 +0000

    Deploy preview for PR #12 🚀

---
+++ preview/12/assets/index-BT3IKBaq.js
@@ -0,0 +1,1 @@
+import{j as t,M as s,T as e}from"./index-BnwwiA8U.js";const i=()=>t.jsx(s,{title:"Settings",children:t.jsx(e,{variant:"body2",children:"Welcome to the Settings. This is a sample page."})});export{i as default};
