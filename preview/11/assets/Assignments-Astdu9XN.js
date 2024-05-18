601f080a644257c719583f4887f5ecdbe33e22b5
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-05-18 11:05:35 +0000

    Deploy preview for PR #11 🚀

--- preview/11/assets/Assignments-Astdu9XN.js
+++
@@ -1,1 +0,0 @@
-import{j as s,T as e}from"./index-B_UubtJt.js";const a=()=>s.jsx(e,{variant:"body2",children:"Welcome to the Assignments. This is a sample page."});export{a as default};
---
+++ preview/11/assets/Seeking-CFTat0H0.js
@@ -0,0 +1,1 @@
+import{j as e,T as a}from"./index-D8wLL1Yt.js";const o=()=>e.jsx(a,{variant:"body2",children:"Welcome to the Seeking. This is a sample page."});export{o as default};
