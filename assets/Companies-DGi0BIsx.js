1e6ae9429f6506e8555fe058a44d37cf6350a6fa
VictorWinberg <victor.m.winberg@gmail.com>
2024-05-19 13:14:06 +0000

    Deploying to gh-pages from @ VictorWinberg/project-x@f946874e58c18adfc5f224cb4753d12a1aad346a 🚀

--- assets/Companies-DGi0BIsx.js
+++
@@ -1,1 +0,0 @@
-import{j as a,T as s}from"./index-fBbRfXtr.js";const o=()=>a.jsx(s,{variant:"body2",children:"Welcome to the Companies. This is a sample page."});export{o as default};
--- assets/Contacts-CWFGkOQn.js
+++
@@ -1,1 +0,0 @@
-import{j as t,T as a}from"./index-fBbRfXtr.js";const o=()=>t.jsx(a,{variant:"body2",children:"Welcome to the Contacts. This is a sample page."});export{o as default};
