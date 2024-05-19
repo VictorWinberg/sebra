a4d31bd20caba7eaf84f64fdbe07025999a077db
VictorWinberg <victor.m.winberg@gmail.com>
2024-05-19 14:41:07 +0000

    Deploying to gh-pages from @ VictorWinberg/project-x@f4b2688bb46207340a042a2f53844aa1080802c1 🚀

--- assets/index-C6PYELHb.js
+++
@@ -1,1 +0,0 @@
-import{j as t,M as s,T as e}from"./index-Bd-1JDTF.js";const i=()=>t.jsx(s,{title:"Settings",children:t.jsx(e,{variant:"body2",children:"Welcome to the Settings. This is a sample page."})});export{i as default};
