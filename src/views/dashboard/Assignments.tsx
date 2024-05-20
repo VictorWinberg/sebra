860995cf0a5aede2df73dae3b7f76456b07e4b70
Victor Winberg <victor.m.winberg@gmail.com>
2024-05-20 17:20:46 +0200

    feat(data-grid): auto width

--- src/views/dashboard/Assignments.tsx
+++ src/views/dashboard/Assignments.tsx
@@ -5,29 +5,21 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 const columns: GridColDef[] = [
   { field: 'name', headerName: 'Uppdragsnamn', width: 160 },
   { field: 'company', headerName: 'Bolag', width: 160 },
-  {
-    field: 'email',
-    headerName: 'Email',
-    width: 230
-  },
+  { field: 'email', headerName: 'Email' },
   {
     field: 'responsible',
     headerName: 'Ansvarig',
-    width: 160,
     sortable: false,
     valueGetter: (_value, row) => `${row.name || ''}`
   },
   {
     field: 'status',
     headerName: 'Status',
-    width: 120,
     sortable: false
   },
   {
     field: 'fee',
     headerName: 'Arvode',
-    width: 120,
-    flex: 1,
     type: 'number',
     renderCell: ({ value }) =>
       Number(value).toLocaleString('sv-SE', {
