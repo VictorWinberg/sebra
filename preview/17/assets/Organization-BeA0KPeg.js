df0b100f57726cffd5354ff3b6ea3a8658144650
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-05-29 20:10:28 +0000

    Deploy preview for PR #17 🚀

---
+++ preview/17/assets/Organization-BeA0KPeg.js
@@ -0,0 +1,1 @@
+import{j as t,T as a}from"./index-CHY1lVvJ.js";const i=()=>t.jsxs(t.Fragment,{children:[t.jsx(a,{variant:"h3",gutterBottom:!0,children:"Organization"}),t.jsx(a,{variant:"body1",children:"Hello, this is the organization page."})]});export{i as default};
