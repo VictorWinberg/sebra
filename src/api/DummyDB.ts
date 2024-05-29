2e71302ebcde3d5bea89f5b06a68acf957bb6bda
Victor Winberg <victor.m.winberg@gmail.com>
2024-05-29 22:09:05 +0200

    fix top-level await

--- src/api/DummyDB.ts
+++ src/api/DummyDB.ts
@@ -1,4 +1,4 @@
-import initSqlJs, { Database } from 'sql.js';
+import initSqlJs, { SqlJsStatic, Database } from 'sql.js';
 import camelcaseKeys from 'camelcase-keys';
 
 import { localStorageSet } from '@/utils';
@@ -6,22 +6,28 @@ import { localStorageSet } from '@/utils';
 import schema from './schema.sql?raw';
 import seed from './seed.sql?raw';
 
-const SQL = await initSqlJs({
-  // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-  locateFile: (file) => `https://sql.js.org/dist/${file}`
-});
-
-// Load a database
-const data = null; // TODO: Use localStorageGet<number[]>('db');
-
+let SQL: SqlJsStatic;
 let db: Database;
-if (data) {
-  db = new SQL.Database(data);
-} else {
-  db = new SQL.Database();
-  db.run(schema);
-  db.run(seed);
+
+async function init() {
+  SQL = await initSqlJs({
+    // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+    locateFile: (file) => `https://sql.js.org/dist/${file}`
+  });
+
+  // Load a database
+  const data = null; // TODO: Use localStorageGet<number[]>('db');
+
+  if (data) {
+    db = new SQL.Database(data);
+  } else {
+    db = new SQL.Database();
+    db.run(schema);
+    db.run(seed);
+  }
 }
+
+init();
 
 export function query<T extends Record<string, unknown>>(sql: string) {
   const [res] = db.exec(sql);
