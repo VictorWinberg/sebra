ee0f1412e72b01127aa6e0fd26e753460d8a7faa
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-05-29 21:41:20 +0000

    Deploy preview for PR #22 🚀

---
+++ preview/22/assets/Leads-C9ODJ5gt.js
@@ -0,0 +1,1 @@
+import{j as a,T as s}from"./index-DdOddnWA.js";const o=()=>a.jsx(s,{variant:"body2",children:"Welcome to the Leads. This is a sample page."});export{o as default};
---
+++ preview/22/assets/Reports-IgSelIme.js
@@ -0,0 +1,1 @@
+import{j as s,T as e}from"./index-DdOddnWA.js";const t=()=>s.jsx(e,{variant:"body2",children:"Welcome to the Reports. This is a sample page."});export{t as default};
