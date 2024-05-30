f1db04c671483e9276f1611f7701e42842c69e08
VictorWinberg <victor.m.winberg@gmail.com>
2024-05-30 14:53:18 +0000

    Deploying to gh-pages from @ VictorWinberg/project-x@2531faa28f8315dc72da19f5727db306eb2e8bf1 🚀

---
+++ assets/Organization-BFL9usDN.js
@@ -0,0 +1,1 @@
+import{j as t,T as a}from"./index-7zkMYh96.js";const i=()=>t.jsxs(t.Fragment,{children:[t.jsx(a,{variant:"h3",gutterBottom:!0,children:"Organization"}),t.jsx(a,{variant:"body1",children:"Hello, this is the organization page."})]});export{i as default};
