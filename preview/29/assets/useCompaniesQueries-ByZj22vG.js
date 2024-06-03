bce29bc0e54f73c3a66c11d97844b832ef83fc3a
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-06-03 10:32:25 +0000

    Deploy preview for PR #29 🚀

---
+++ preview/29/assets/useCompaniesQueries-ByZj22vG.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useQuery-DKV-AIIa.js";import{f as r}from"./companiesApi-CUm0jIpn.js";const u=()=>e({queryKey:["companies"],queryFn:r});export{u};
---
+++ preview/29/assets/useContactsQueries-Qw2Y-qk9.js
@@ -0,0 +1,1 @@
+import{u as t}from"./useQuery-DKV-AIIa.js";import{f as r}from"./contactsApi-D-yqP3ow.js";const s=()=>t({queryKey:["contacts"],queryFn:r});export{s as u};
