88a449789055bcb10be3415f4d7ec84a3f272661
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-06-10 14:52:50 +0000

    Deploy preview for PR #31 🚀

---
+++ preview/31/assets/useCompaniesQueries-DmKSoOLq.js
@@ -0,0 +1,1 @@
+import{u as e}from"./useQuery-D691ff4D.js";import{f as r}from"./companiesApi-C-vLZiTt.js";const u=()=>e({queryKey:["companies"],queryFn:r});export{u};
---
+++ preview/31/assets/useContactsQueries-DFFPpVPQ.js
@@ -0,0 +1,1 @@
+import{u as t}from"./useQuery-D691ff4D.js";import{f as r}from"./contactsApi-B8YJsCMW.js";const s=()=>t({queryKey:["contacts"],queryFn:r});export{s as u};
