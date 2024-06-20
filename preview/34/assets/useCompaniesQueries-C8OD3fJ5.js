8a483e24f4d4ef46ea631bceab1540ab4fb2c737
VictorWinberg <VictorWinberg@users.noreply.github.com>
2024-06-20 14:44:07 +0000

    Deploy preview for PR #34 🚀

---
+++ preview/34/assets/useCompaniesQueries-C8OD3fJ5.js
@@ -0,0 +1,1 @@
+import{u as e}from"./Autocomplete-DzWT55fa.js";import{f as r}from"./companiesApi-DlYb7tpE.js";const u=()=>e({queryKey:["companies"],queryFn:r});export{u};
---
+++ preview/34/assets/useContactsQueries-DK6NcWqa.js
@@ -0,0 +1,1 @@
+import{u as t}from"./Autocomplete-DzWT55fa.js";import{f as r}from"./contactsApi-DlKDKdVz.js";const s=()=>t({queryKey:["contacts"],queryFn:r});export{s as u};
