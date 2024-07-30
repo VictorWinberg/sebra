9fab77d546a94373f505c7488832b6f361555829
Victor Winberg <victor.m.winberg@gmail.com>
2024-07-30 13:00:46 +0200

    feat(query-params): add persistent filters

--- src/hooks/useQueryParam.ts
+++ src/hooks/useQueryParam.ts
@@ -1,9 +1,25 @@
-import { useMemo } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+// material-ui
+import { MRT_ColumnFiltersState } from 'material-react-table';
+
+// third-party
+import queryString from 'query-string';
 
 type Parser<T> = {
   parse: (value: string) => T;
   serialize: (value: T) => string;
+};
+
+export const FilterParam: Parser<MRT_ColumnFiltersState> = {
+  parse: (value) => {
+    const parsed = queryString.parse(value, { arrayFormat: 'separator' });
+    return Object.entries(parsed).map(([id, value]) => ({ id, value }));
+  },
+  serialize: (filters) => {
+    const transformed = filters.reduce((acc, { id, value }) => ({ ...acc, [id]: value }), {});
+    return queryString.stringify(transformed, { arrayFormat: 'separator' });
+  }
 };
 
 export const StringParam: Parser<string> = {
@@ -16,27 +32,30 @@ export const NumberParam: Parser<number> = {
   serialize: (value) => value.toString()
 };
 
-export const useQueryParam = <T>(
-  key: string,
-  parser: Parser<T>,
-  initialValue?: T
-): [T | null, (value: T | null) => void] => {
-  const [searchParams, setSearchParams] = useSearchParams();
+const useUpdateQueryStringValueWithoutNavigation = (queryKey: string, queryValue: string) => {
+  useEffect(() => {
+    const currentSearchParams = new URLSearchParams(window.location.search);
+    const oldQuery = currentSearchParams.get(queryKey) ?? '';
+    if (queryValue === oldQuery) return;
 
-  const queryParam = useMemo(() => {
-    const value = searchParams.get(key);
-    return value !== null ? parser.parse(value) : initialValue ?? null;
-  }, [key, parser, initialValue, searchParams]);
-
-  const setQueryParam = (newValue: T | null) => {
-    if (newValue !== null) {
-      const serializedValue = parser.serialize(newValue);
-      searchParams.set(key, serializedValue);
+    if (queryValue) {
+      currentSearchParams.set(queryKey, queryValue);
     } else {
-      searchParams.delete(key);
+      currentSearchParams.delete(queryKey);
     }
-    setSearchParams(searchParams);
-  };
+    const newUrl = [window.location.pathname, currentSearchParams.toString()].filter(Boolean).join('?');
+    window.history.replaceState(null, '', newUrl);
+  }, [queryKey, queryValue]);
+};
+
+export const useQueryParam = <T>(key: string, parser: Parser<T>, initialValue: T): [T, Dispatch<SetStateAction<T>>] => {
+  const [queryParam, setQueryParam] = useState(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const value = searchParams.get(key);
+    return value !== null ? parser.parse(value) : initialValue;
+  });
+
+  useUpdateQueryStringValueWithoutNavigation(key, parser.serialize(queryParam));
 
   return [queryParam, setQueryParam];
 };
