9ebf1da0439bc0ad5d57ec92fef4ad69a93d72ac
Victor Winberg <victor.m.winberg@gmail.com>
2024-08-14 11:12:15 +0200

    refactor: interaction contacts (#95)

--- src/features/interactions/hooks/useInteractionsMutations.ts
+++ src/features/interactions/hooks/useInteractionsMutations.ts
@@ -1,14 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { useSnackbar } from '@/hooks/useSnackbar';
-import {
-  createInteraction,
-  createInteractionContacts,
-  deleteInteraction,
-  deleteInteractionContacts,
-  updateInteraction,
-  updateInteractionContacts
-} from '../api/interactionsApi';
+import { createInteraction, deleteInteraction, updateInteraction } from '../api/interactionsApi';
 
 export const useCreateInteraction = () => {
   const queryClient = useQueryClient();
@@ -47,46 +40,13 @@ export const useDeleteInteraction = () => {
   const { showSnackbar } = useSnackbar();
 
   return useMutation({
-    mutationFn: (params: { interactionId: string }) =>
-      Promise.all([deleteInteraction(params), deleteInteractionContacts(params)]),
+    mutationFn: deleteInteraction,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['interactions'] });
       showSnackbar('Interaktion borttaget!');
     },
     onError: () => {
       showSnackbar('Ett fel uppstod när interaktionen skulle tas bort.', 'error');
-    }
-  });
-};
-
-export const useCreateInteractionContacts = () => {
-  const queryClient = useQueryClient();
-  const { showSnackbar } = useSnackbar();
-
-  return useMutation({
-    mutationFn: createInteractionContacts,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['interactions'] });
-      showSnackbar('Interaktion skapad!');
-    },
-    onError: () => {
-      showSnackbar('Ett fel uppstod när interaktionen skulle skapas.', 'error');
-    }
-  });
-};
-
-export const useUpdateInteractionContacts = () => {
-  const queryClient = useQueryClient();
-  const { showSnackbar } = useSnackbar();
-
-  return useMutation({
-    mutationFn: updateInteractionContacts,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['interactions'] });
-      showSnackbar('Interaktion uppdaterad!');
-    },
-    onError: () => {
-      showSnackbar('Ett fel uppstod när interaktionen skulle uppdateras.', 'error');
     }
   });
 };
