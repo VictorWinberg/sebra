7b1a2f17c5efa131bcee77cb1838dcacc140cddc
Victor Winberg <victor.m.winberg@gmail.com>
2024-12-11 21:17:20 +0100

    feat(workspaces): add workspaces

--- src/features/authentication/hooks/useAuthMutations.ts
+++ src/features/authentication/hooks/useAuthMutations.ts
@@ -19,6 +19,10 @@ export const useAuthLogin = () => {
               user {
                 id
                 email
+                workspaces {
+                  id
+                  name
+                }
               }
             }
           }
