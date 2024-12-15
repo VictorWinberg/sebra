36fc8857cd105b70155df2632fa83cdccd6f4ba0
Victor Winberg <victor.m.winberg@gmail.com>
2024-12-15 19:41:19 +0100

    feat(workspace): send workspace header (#116)
    * feat(workspaces): add workspaces

--- src/ui-component/RouterLink.tsx
+++ src/ui-component/RouterLink.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { LinkProps, Link, useParams } from 'react-router-dom';
 
-export const RouterLink: React.FC<LinkProps> = ({ to, ...props }) => {
+export const RouterLink = React.forwardRef<HTMLAnchorElement, LinkProps>(({ to, ...props }, ref) => {
   const { workspace } = useParams();
   const workspacePath = `/${workspace}${to}`;
 
-  return <Link to={workspacePath} {...props} />;
-};
+  return <Link to={workspacePath} ref={ref} {...props} />;
+});
